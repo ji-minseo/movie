0b233d8d542c9ae7306b514a99b8a33166a06aa8
ji-minseo <you@example.com>
2020-09-06 20:18:56 +0900

    try adding new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cmp from './Cmp';
 
 function App() {
   return (
     <div className="App">
-      
+      HIHI APP
+      <Cmp />
     </div>
   );
 }
---
+++ src/Cmp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Cmp() {
+    return <h1>HIHI</h1>;
+}
+export default Cmp;
